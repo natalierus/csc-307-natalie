8aa157e3e30593102b313e3677dbda58db17ef53
NatalieRusin <nrusin@calpoly.edu>
2025-10-07 16:04:40 -0700

    getting users by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -27,7 +27,7 @@ const users = {
     },
     {
       id: "zap555",
-      name: "Dennis",
+      name: "Dennnynynyny",
       job: "Bartender"
     }
   ]
@@ -35,13 +35,26 @@ const users = {
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  res.send(users);
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
 
 app.listen(port, () => {
   console.log(
